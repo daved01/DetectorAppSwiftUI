f5ee25f0da4874ff753af669e8a861a5151b6b23
daved01 <david.kirchhoff@icloud.com>
2022-08-07 11:26:06 +0200

    Initial Commit

---
+++ ObjectDetectorSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObjectDetectorSwiftUI
+//
+//  Created by David Kirchhoff on 2022-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ObjectDetectorSwiftUI/ObjectDetectorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectorSwiftUIApp.swift
+//  ObjectDetectorSwiftUI
+//
+//  Created by David Kirchhoff on 2022-08-07.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
